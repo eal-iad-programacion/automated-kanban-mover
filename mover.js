e6ef2e64757c6c73ab643fc85abd2c22c5ab98b3
eal-iad-programaciona <eal.iad.programacion@gmail.com>
2019-05-17 13:41:17 -0500

    Se creo la clase close #2
    primer commit relacionado al proyecto mover

---
+++ mover.js
@@ -0,0 +1,19 @@
+// The Nature of Code
+// Daniel Shiffman
+// http://natureofcode.com
+
+class Mover {
+  constructor() {
+  }
+
+  applyForce(force) {
+  }
+
+  update() {
+  }
+
+  display() {
+  }
+
+
+}
